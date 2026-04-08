f003ee1c0d0861191c8b53d3b15651b327bc1f18
Luis Alvarado Arias <lalvarado@officespacesoftware.com>
2026-04-08 10:31:26 -0600

    Refactor server.js to include new API routes for categorias, productos, carrito, and ordenes; remove deprecated departamento route; improve code formatting for better readability.

--- Backend/server.js
+++ Backend/server.js
@@ -5,27 +5,32 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-//Luego agregamos info de DB
-
-// Aqui van las rutas de la API
+const authRoutes = require('./routes/auth.route');
 const usuariosRoutes = require('./routes/usuarios.route');
-const departamentosRoutes = require('./routes/departamento.route');
+const categoriasRoutes = require('./routes/categorias.route');
+const productosRoutes = require('./routes/productos.route');
+const carritoRoutes = require('./routes/carrito.route');
+const ordenesRoutes = require('./routes/ordenes.route');
 
 const app = express();
 
 mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Error connecting to MongoDB:', err));
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/auth', authRoutes);
 app.use('/api/usuarios', usuariosRoutes);
-app.use('/api/departamentos', departamentosRoutes);
+app.use('/api/categorias', categoriasRoutes);
+app.use('/api/productos', productosRoutes);
+app.use('/api/carrito', carritoRoutes);
+app.use('/api/ordenes', ordenesRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
-
